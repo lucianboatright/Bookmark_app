df10a69154a1011551d168b5c5bd43c4e7f85110
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-15 18:14:54 +0000

    changed methods to class methods in tests

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -4,16 +4,17 @@ describe Bookmarks do
 
   describe '#all' do
     it 'shows all bookmarks' do
+
       connection = PG.connect(:dbname => 'bookmark_manager_test')
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.bbc.com');")  
-      expect(subject.all).to include 'http://www.bbc.com'
+      expect(Bookmarks.all).to include 'http://www.bbc.com'
     end
   end
 
   describe '#add' do
     it 'added bookmarks are stores in database' do
-      subject.add('http://www.bunnies.com')
-      expect(subject.all).to include 'http://www.bunnies.com'
+      Bookmarks.add('http://www.bunnies.com')
+      expect(Bookmarks.all).to include 'http://www.bunnies.com'
     end
   end
 end
